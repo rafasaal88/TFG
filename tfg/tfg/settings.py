87fd07f83ebd8c602aebbb7f13970a38e92b68e2
rafasaal88 <rafasaal88@gmail.com>
2019-12-04 17:03:42 +0100

    update

--- tfg/tfg/settings.py
+++ tfg/tfg/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'backend',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -73,10 +75,18 @@ WSGI_APPLICATION = 'tfg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tfg',
+        'USER': 'tfg',
+        'PASSWORD': 'tfg',
+        'HOST': 'localhost',
+        'PORT': '',
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+        }
     }
 }
 
