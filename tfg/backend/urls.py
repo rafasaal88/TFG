3f7febbcf227f982458d936ca8e4fac1119145c1
rafasaal88 <rafasaal88@gmail.com>
2019-12-06 19:24:15 +0100

    create crud for company

--- tfg/backend/urls.py
+++ tfg/backend/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-from .views import create_company, home
+from .views import create_company, home, list_company, edit_company, delete_company
 
 urlpatterns = [
     url(r'^$', home, name='index'),
-    url(r'^company_form/',create_company, name = "create_company")
+    url(r'^create_company/',create_company, name = "create_company"),
+    url(r'^list_company/',list_company, name = "list_company"),
+    url(r'^edit_company/(?P<id>\d+)/$',edit_company, name = "edit_company"),
+    url(r'^delete_company/(?P<id>\d+)/$',delete_company, name = "delete_company")
 ]
--- tfg/backend/views.py
+++ tfg/backend/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 
@@ -17,3 +17,26 @@ def create_company(request):
     else:
         form = CompanyForm()
     return render(request,'backend/create_company.html',{'form':form})
+
+def list_company(request):
+    company = Company.objects.all()
+    context = {'company':company}
+    return render(request, 'backend/list_company.html', context)
+
+def edit_company(request, id):
+    company = Company.objects.get(id =id)
+    if request.method == 'GET':
+        form = CompanyForm(instance = company)
+    else:
+        form = CompanyForm(request.POST, instance=company)
+        if form.is_valid():
+            form.save()
+        return redirect('index')
+    return render(request,'backend/create_company.html', {'form':form})
+
+def delete_company(request, id):
+    company = Company.objects.get(id =id)
+    if request.method == 'POST':
+        company.delete()
+        return redirect('index')
+    return render(request,'backend/delete_company.html', {'company':company})
