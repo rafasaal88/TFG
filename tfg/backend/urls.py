947fe30f5c32912ee4e0d6ff7573e107df4d696c
rafasaal88 <rafasaal88@gmail.com>
2019-12-07 19:39:40 +0100

    add login/logout

--- tfg/backend/urls.py
+++ tfg/backend/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
-from .views import create_company, home, list_company, edit_company, delete_company
+from .views import create_company, home, list_company, edit_company, delete_company, login, logout
 
 urlpatterns = [
     url(r'^$', home, name='index'),
+    url(r'^login/',login, name = 'login'),
+    url(r'^logout/',logout, name = 'logout'),
     url(r'^create_company/',create_company, name = "create_company"),
     url(r'^list_company/',list_company, name = "list_company"),
     url(r'^edit_company/(?P<id>\d+)/$',edit_company, name = "edit_company"),
-    url(r'^delete_company/(?P<id>\d+)/$',delete_company, name = "delete_company")
+    url(r'^delete_company/(?P<id>\d+)/$',delete_company, name = "delete_company"),
 ]
--- tfg/backend/views.py
+++ tfg/backend/views.py
@@ -2,11 +2,57 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 
+from django.contrib.auth import login as do_login
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate, logout, get_user_model
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout as django_logout
+
 from .models import Company
 from .forms import CompanyForm
 
 def home(request):
-    return render(request,'index.html')
+    if request.user.is_authenticated:
+        return render(request, 'backend/index.html')
+    # En otro caso redireccionamos al login
+    return redirect('login')
+
+def login(request):
+    if request.user.is_authenticated:
+        return render(request, 'backend/index.html')
+    # Creamos el formulario de autenticación vacío
+    form = AuthenticationForm()
+    if request.method == "POST":
+        # Añadimos los datos recibidos al formulario
+        form = AuthenticationForm(data=request.POST)
+        # Si el formulario es válido...
+        if form.is_valid():
+            # Recuperamos las credenciales validadas
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            # Verificamos las credenciales del usuario
+            user = authenticate(username=username, password=password)
+
+            # Si existe un usuario con ese nombre y contraseña
+            if user is not None:
+                # Hacemos el login manualmente
+                do_login(request, user)
+                # Y le redireccionamos a la portada
+                return HttpResponseRedirect('index')
+
+    # Si llegamos al final renderizamos el formulario
+    return render(request, 'backend/login.html', {'form': form})
+
+
+def logout(request):
+    # Finalizamos la sesión
+    django_logout(request)
+    # Redireccionamos a la portada
+    return redirect('login')
+
+
 
 def create_company(request):
     if request.method == 'POST':
@@ -17,6 +63,9 @@ def create_company(request):
     else:
         form = CompanyForm()
     return render(request,'backend/create_company.html',{'form':form})
+
+
+
 
 def list_company(request):
     company = Company.objects.all()
