74fee1261c29650b9401310b21e033c3c876cc84
rafasaal88 <rafasaal88@gmail.com>
2020-01-21 20:53:03 +0100

    fix templates responsive and widgets forms

--- tfg/backend/forms.py
+++ tfg/backend/forms.py
@@ -11,3 +11,10 @@ class Publicity_Campaign_Form(forms.ModelForm):
             'description',
             'name',
         ]
+        widgets = {
+        'name': forms.TextInput(attrs={'class': 'form-control'}),
+        'description': forms.Textarea(attrs={'class': 'form-control'}),
+        'date_start': forms.DateInput(attrs={'class': 'form-control'}),
+        'date_end': forms.DateInput(attrs={'class': 'form-control'}),
+       
+    }
